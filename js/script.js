6f4a812372ecdeae0326cb58841d28af1421ed6a
Max <acidrix@live.fr>
2022-08-09 13:41:28 +0200

    article index

--- js/script.js
+++ js/script.js
@@ -1,14 +1,20 @@
+//fonction pour ajouter les element article ; img; name et description au html// 
+
 function newProduct(imageUrl, altTxt, name, description) {
     const newItems = document.createElement('article');
     newItems.innerHTML = `<img src="${imageUrl}" alt="${altTxt}">
                           <h3 class="productName">${name}</h3>
                           <p class="productDescription">${description}</p>`;
     document.querySelector('.items').appendChild(newItems);
-
 }
+//fonction pour ajouter les lien de chaques articles//
 
+
+
+// fonction async pour recuperer les donnes de l'api//
 async function getProducts() {
     let productsArray =  await fetch("http://localhost:3000/api/products")
+    
     .then(res => res.json());
 
     console.log(productsArray)
